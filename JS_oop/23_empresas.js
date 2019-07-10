4c4a78b370468e567a936b42fbec8002328be249
CursosTardes <CursosTardes@tic.loc>
2019-07-10 20:40:43 +0200

    todo de facturas

---
+++ JS_oop/23_empresas.js
@@ -0,0 +1,42 @@
+
+module.exports = {}
+
+module.exports.Bighit = new empresa(
+    'Bighit', 
+    new direccion('c/ Bangtan', '7', 'Seul', 'Corea del Sur'), 
+    '625872321', 
+    'T-3476179834'
+)
+
+module.exports.Army = new empresa (
+    'Army', 
+    new direccion('c/ Fan de Bts', '35', 'Madrid', 'España'), 
+    '625667881', 
+    'T-347639454'
+)
+
+module.exports
+
+/*
+const { Empresa, Direccion } = require('./23_modelos.js')
+
+module.exports = {}
+
+module.exports.tuata = new Empresa(
+    'Ediciones Tuatalug',
+    new Direccion('c/ Pez', '1', 'Madrid', 'España'),
+    '625875921',
+    'T-34761234',)
+
+module.exports.boracai = new Empresa(
+        'Librería Boracai',
+        new Direccion ('c/ Carranza', '5', 'Cádiz', 'España'),
+        '625493421',
+        'F-76521743')
+
+module.exports.pepita = new Empresa(
+            'Librería Pepita',
+            new Direccion ('c/ Burgo', '5', 'Estrasburgo', 'Francia'),
+            '625492190',
+            'F-76534127')
+            */
--- JS_oop/23_factura_lucia.js
+++ JS_oop/23_factura_lucia.js
@@ -5,33 +5,8 @@ utilizar siempre que puedas prototype
 añadir el objeto cliente y array de objetos de elementos
 
 */
-/*
+
 const {fechaEsp, fijarLongitudCadena} = require('./15_helpers.js')
-
-const templateFactura = {
-    empresa: {
-        nombre: '',
-        direccion: '',
-        telefono: '',
-        nif: '',
-    },
-    cliente: {
-        nombre: '',
-        direccion: '',
-        telefono: '',
-        nif: '',
-    },
-    items : [
-        {descripcion: '', precioU: 0, cantidad: 0} 
-    ],
-    numFactura: '',
-    tipoIVA: '',
-    formaPago: '',
-    fecha: new Date()
-    }
-
-templateFactura.calcularImporte = function() {}
-templateFactura.mostrarImporte = function() {}*/
 
 function Direccion(calle, numero, ciudad, pais) {
     this.calle = calle
@@ -40,51 +15,48 @@ function Direccion(calle, numero, ciudad, pais) {
     this.pais = pais
 }
 
-function factura(empresa, cliente, items){
+direccion.prototype.toString = function() {
+    return `${this.calle} ${this.numero} ${this.ciudad} - ${this.pais}`
+}
+
+function Factura(empresa, cliente, items, numFactura, tipoIVA, formaPago){
     this.empresa = empresa
     this.cliente = cliente
     this.items = items
     this.fecha = fecha
+    this.numFactura = numFactura
+    this.tipoIVA = tipoIVA
+    this.formaPago = formaPago
 }
 
-function empresa(nombre, direccion, telefono, nif) {
+function Empresa(nombre, direccion, telefono, nif) {
     this.nombre = nombre
     this.direccion = direccion 
     this.telefono = telefono
     this.nif = nif 
 }
 
-function cliente(nombre, direccion, telefono, nif) {
+function Cliente(nombre, direccion, telefono, nif) {
     this.nombre = nombre
     this.direccion = direccion 
     this.telefono = telefono
     this.nif = nif
 }
 
-function items(descripcion, precioU, cantidad) {
+function Items(descripcion, precioU, cantidad) {
     this.descripcion = descripcion
     this.precioU = precioU 
     this.cantidad = cantidad
 }
 
-
-const empresa1 = new empresa('Bighit', 'c/ Bangtan', 625872321, 'T-3476179834',)
-const cliente1 = new cliente('Army', 'c/ Fan de Bts', 625667881, 'T-34763454',)
-const items1 = new items('Lighstick con la forma del mundo', 80, 100)
-
-console.log(empresa)
-
-empresa1.vender('Lighstick con la forma del mundo')
+Factura.prototype.calcularImporte = function() {}
 
 
 
-
-
-/*
-factura.calcularImporte = function() {
+Factura.prototype.calcularImporte = function() {
     const importe = {} 
     importe.base = 
-        this.items
+        this.Items
         .map( libro => libro.precioU * libro.cantidad)
         .reduce( (total, item) => total + item ) 
     importe.iva = importe.base * this.tipoIVA
@@ -92,8 +64,8 @@ factura.calcularImporte = function() {
     return importe
 }
 
-factura.listarItems = function() {
-    let items = ``
+Factura.prototype.listarItems = function() {
+    let Items = ``
     this.items.forEach(
         item => {
             let desc = fijarLongitudCadena(item.descripcion, 30)
@@ -108,7 +80,9 @@ factura.listarItems = function() {
     return items
 }
 
-factura.prepararFactura = function() {
+Factura.prototype.direcciontoString()
+
+Factura.prototype.prepararFactura = function() {
     const importe = this.calcularImporte()
     const factura =
 `
@@ -138,9 +112,237 @@ factura.prepararFactura = function() {
     return factura
 }
 
-factura.render = function () {
+Factura.prototype.render = function () {
     console.log(this.prepararFactura())
 }
 
+Factura.prototype.resumir = function () {
+    console.log(
+        `factura: ${this.numFactura}: `
+    )
+    this.calcularImporte().total
 
-factura.render()*/
+}
+
+const Empresa = new Empresa('Bighit', 
+        new direccion('c/ Bangtan', '7', 'Seul', 'Corea del Sur'), 
+        '625872321', 
+        'T-3476179834')
+const Cliente1 = new Empresa('Army', 
+        new direccion('c/ Fan de Bts', '35', 'Madrid', 'España'), 
+        '625667881', 
+        'T-347639454')
+const Items = [
+        new items('Lighstick con la forma del mundo', 80, 100),
+        new items('Album Persona', 30, 500),
+        new items('Camiseta con los dibujos de BT21', 40, 200)
+] 
+const Factura_59 = new factura(
+        empresa, cliente1, items, '000000059',
+        0.04, 'contado', new Date()
+)
+
+//factura_59.render()
+console.log(Factura_59)
+
+
+/* 
+Definir la estructura de una funció constructora 
+ que crea los objetos que almacenan una factura. 
+Las facturas están formadas por:
+- la información de la propia empresa:
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- la información del cliente
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- una lista de elementos, cada uno de los cuales dispone de:
+    - descripción
+    - precio por unidad
+    - cantidad 
+- otra información básica de la factura
+    - el número de factura
+    - tipo de IVA
+    - forma de pago
+    - la fecha
+Añadir al menos dos métodos:
+- el que calcula el importe total de la factura
+- el que muestra por pantalla la factura 
+    o al menos el importe total de la factura.
+*/
+/*
+const {fechaEsp, fijarLongitudCadena} = require('./15_helpers.js')
+
+function Factura (  empresa = {}, 
+                    cliente = {},
+                    items = [{}],
+                    numFactura = '',
+                    tipoIVA = '',
+                    formaPago = '',
+                    fecha = new Date()) {
+    this.empresa = empresa
+    this.cliente = cliente
+    this.items = items
+    this.numFactura = numFactura
+    this.tipoIVA = tipoIVA
+    this.formaPago = formaPago
+    this.fecha = fecha
+}
+
+function Empresa (nombre, direccion, telefono, nif) {
+        this.nombre = nombre
+        this.direccion = direccion
+        this.telefono = telefono
+        this.nif = nif
+}
+
+function Item (descripcion = '', precioU = 0, cantidad = 0) {
+    this.descripcion = descripcion
+    this.precioU = precioU
+    this.cantidad = cantidad
+}
+
+function Direccion(calle, num, poblacion, pais) {
+    this.calle = calle
+    this.num = num
+    this.poblacion = poblacion
+    this.pais = pais 
+}
+
+Direccion.prototype.toString = function () {
+    return `${this.calle} ${this.num} ${this.poblacion} - ${this.pais}`
+}
+
+Factura.prototype.calcularImporte = function() {
+    const importe = {} 
+    importe.base = 
+        this.items
+        .map( libro => libro.precioU * libro.cantidad)
+        .reduce( (total, item) => total + item ) 
+    importe.iva = importe.base * this.tipoIVA
+    importe.total = importe.base + importe.iva
+    return importe    
+}
+
+Factura.prototype.listarItems = function() {
+    let items = ``
+    this.items.forEach(
+        item => {
+            let desc = fijarLongitudCadena(item.descripcion, 30)
+            let pv = item.precioU
+                .toLocaleString('es', {style: 'currency', currency: 'EUR'})
+            let cant = item.cantidad
+            items += 
+`
+    ${desc}     ${pv}          ${cant}
+`
+    })
+    return items
+}
+
+Factura.prototype.prepararFactura = function() {
+    const importe = this.calcularImporte()
+    const factura =
+`
+    _______________________________________________________________
+                                        ${this.empresa.nombre}
+                                        ${this.empresa.direccion}
+                                        ${this.empresa.telefono}
+                                        ${this.empresa.nif}
+    Num. Factura: ${this.numFactura}
+    ${this.cliente.nombre}
+    ${this.cliente.direccion}
+    ${this.cliente.telefono}
+    ${this.cliente.nif}
+    Descripción                         Precio      Cantidad
+    _______________________________     _______     ________
+    ${this.listarItems()}
+    Importe ....................................${importe.base
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    IVA ........................................${importe.iva
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    Importe Total ..............................${importe.total
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    Forma de Pago ..............................${this.formaPago}
+    Madrid, ${fechaEsp(this.fecha)}
+    ______________________________________________________________
+`
+    return factura
+}
+
+// Factura.prototype.mostrarImporte = function() {}
+
+Factura.prototype.render = function () {
+    console.log(this.prepararFactura())
+}
+
+Factura.prototype.resumir = function () {
+    console.log (
+`Factura: ${this.numFactura}: ${this.calcularImporte().total}`
+    )
+}
+
+const tuata = new Empresa(
+        'Ediciones Tuatalug',
+        new Direccion('c/ Pez', '1', 'Madrid', 'España'),
+        '625875921',
+        'T-34761234',)
+
+const cliente1 = new Empresa(
+        'Librería Boracai',
+        new Direccion ('c/ Carranza', '5', 'Cádiz', 'España'),
+        '625493421',
+        'F-76521743')
+
+const items1 = [
+    new Item('Angular 8.0', 30, 10),
+    new Item('JS para torpes', 32, 12),
+    new Item('Typescrip Avanzado', 45, 8),
+    new Item('Introducción a las aplicaciones Web', 37, 11)
+]
+
+const factura_12 = new Factura(
+    tuata, cliente1, items1, '000000012',
+    0.04, 'Contado', new Date()
+)
+
+factura_12.render()
+
+
+const cliente2 = new Empresa(
+    'Librería Pepita',
+    new Direccion ('c/ Burgo', '5', 'Estrasburgo', 'Francia'),
+    '625492190',
+    'F-76534127')
+
+const items2 = [
+    new Item('Angular 8.0', 30, 20),
+    new Item('Typescrip Avanzado', 45, 12),
+    new Item('Introducción a las aplicaciones Web', 37, 14)
+]
+
+const factura_13 = new Factura(
+    tuata, cliente2, items2, '000000013',
+    0.04, 'Transferencia', new Date()
+)
+
+factura_13.render()
+
+new Factura (
+    new Empresa( 'Ediciones Tuatalug',
+    new Direccion('c/ Pez', '1', 'Madrid', 'España'),
+    '625875921',
+    'T-34761234',),
+    new Empresa( 'Librería Pepita',
+    new Direccion ('c/ Burgo', '5', 'Estrasburgo', 'Francia'),
+    '625492190',
+    'F-76534127'),
+    [new Item('L1', 40, 12), new Item('L2', 50, 6), 
+    new Item('L3', 56, 17)], '000000014', 0.04, 
+    'Efectivo', new Date()
+).resumir()
+*/
